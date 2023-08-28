cab1772addab836ffeab5393abfb5c1987117407
solee <dpthf9596@naver.com>
2023-08-28 23:32:50 +0900

    add array

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const amIFat = true;
 console.log(amIFat);
+
+const days0fweek=["mon", "tue", "wed", "thu", "fri", "sat"];
+
+//get Item form array
+console.log(days0fweek);
+
+// Add one more day to the array
+days0fweek.push("sun");
+console.log(days0fweek);
