873522f9cf8e04c294c124cab5eb288e6218a6f9
yesolee97 <dpthf9596@naver.com>
2024-05-14 18:03:00 +0900

    240514 clock studied2

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,4 @@
 const clock = document.querySelector("#clock");
-//clock.innerText = "lalal"
-
 
 function sayHello(){
     console.log("hello");
@@ -8,10 +6,17 @@ function sayHello(){
 
 //setInterval(sayHello, 5000); //5초마다 반복실행, 사이트 접속 5초후 실행
 //setTimeout(sayHello, 5000); // 5초 기다렸다가 한번만 실행, 사이트 접속 5초후 실행
+//PadStart (20, "x") // 앞쪽에 20글자가 아닐경우 x로 채워넣는다
 
 function getclock(){
     const date = new Date();
-    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+    const hours = String(date.getHours()).PadStart(2, "0");
+    const minutes = String(date.getgetMinutesHours()).PadStart(2, "0");
+    const second = String(date.getSeconds()).PadStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${second}`;
+    date.getSeconds()
 }
 getclock();// 바로 시간 호출하기 위한 함수
 setInterval(getclock, 1000);// 1초마다 호출
+
