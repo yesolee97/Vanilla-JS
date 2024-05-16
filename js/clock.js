5506f2116119a4a6dfcfca4aabd25cf78f32c9eb
yesolee97 <dpthf9596@naver.com>
2024-05-16 11:50:16 +0900

    240516 clock End of study

--- js/clock.js
+++ js/clock.js
@@ -9,13 +9,12 @@ const clock = document.querySelector("#clock");
 //PadStart (20, "x") // 앞쪽에 20글자가 아닐경우 x로 채워넣는다
 
 function getclock(){
-    const date = new Date();
+    const date = new Date();// 코드를 실행한 시점의 시간, 분, 초 등을 알려줌
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padEnd(3, "무래");
 
     clock.innerText = `${hours}:${minutes}:${seconds}`;
-    date.getSeconds()
 }
 getclock();// 바로 시간 호출하기 위한 함수
 setInterval(getclock, 1000);// 1초마다 호출
