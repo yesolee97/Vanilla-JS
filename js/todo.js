5e1b879c7b45ca95f0c41013b9c597902fbdc629
yesolee97 <dpthf9596@naver.com>
2024-05-21 13:37:35 +0900

    240521 doto list ~#7.4 study (very difficult.......)

--- js/todo.js
+++ js/todo.js
@@ -2,18 +2,20 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
-const toDos = []
+const TODOS_KEY = "todos";
+
+//Array 만들기 (배열)
+const toDos = [];
 
 //toDos array를 로컬스토리지에 집어넣기 
 function saveToDo(){
-    localStorage.setItem("todos", toDos);
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //배열을 스트링 값으로 변환해 스토리지에 저장 > 로컬스토리지에 저장할 때 스트링값만 저장할 수 있음, 
 }
 
 function deleteToDo(event){
     console.log("test");
     const li = event.target.parentElement; // 클릭된 이벤트 타겟(버튼) > parentElement 버튼의 부모(li)를 가르킴
     li.remove(); // 가르키고있는 li를 삭제하겠다
-
 }
 
 function paintToDo(newTodo){
@@ -28,13 +30,26 @@ function paintToDo(newTodo){
     toDoList.appendChild(li);  // 만들어둔 ul 코드 안에 최종 li 코드 추가
 }
 
-function handleToDoSubmit(event){
-    event.preventDefault();
+function handleToDoSubmit(e){
+    e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = ""; // 이코드를 실행한다고 해서 위에서 실행한 newTodo의 값이 비워지지는 않음 > 위 시점에서 toDoInput.value값을 newTodo에 저장해놓았기 때문에.
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    toDos.push(newTodo); //배열에 newTodo 값 집어넣기
+    paintToDo(newTodo); //paintToDo에 newTodo 값 넘겨주기 (넘겨준 paintToDo값으로 paintToDo안에 코드에서 돌아가서 결과값 출력)
     saveToDo();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+const getToDos = localStorage.getItem(TODOS_KEY); // 스토리지에 저장되어 있는 스트링 값 불러옴
+
+if(getToDos !== null){
+    const parsedToDos = JSON.parse(getToDos); // 위에서 불러온 getToDos값을 배열로 불러올 수 있게 JSPN으로 처리
+    console.log(parsedToDos);
+    //parsedToDos.forEach(sayHello); //배열갯수만큼 sayHello 펑션 돌림(일종의 반복문) + function sayHello(item){ 와 세트
+    parsedToDos.forEach((item) => console.log("this is the turn of", item)); // 윗줄을 좀 더 간결하게 써준것
+}
+/* function sayHello(item){
+    console.log("this is the turn of", item); //this is the turn of + 배열값 출력
+}*/
